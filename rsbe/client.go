9253ec5038d5dc316a8e4d5f9f78588009fd08a9
jgpawletko <jgpawletko@gmail.com>
2020-06-22 16:44:30 -0400

    refactor rsbe/client.go
    * Remove local initialization using environment variables because this
      code will likely be used as a library.
    * Change rsbe.Config type to public visibility
    * Change rsbe.Config attributes to public visibility
    * Add a rsbe.ConfigureClient function to allow library users to
      configure the rsbe.client with information from their environment
    * refactored rsbe.GetBodyTextString to use existing rsbe.Get function

--- rsbe/client.go
+++ rsbe/client.go
@@ -2,23 +2,25 @@ package rsbe
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 type Config struct {
-	rsbe_url      string
-	rsbe_user     string
-	rsbe_password string
+	BaseURL  string
+	User     string
+	Password string
 }
 
-var conf Config
+var conf *Config
 var client *http.Client
 
+func ConfigureClient(c *Config) {
+	conf = c
+}
+
 func init() {
-	init_global_config()
 	client = &http.Client{}
 }
 
@@ -32,33 +34,20 @@ func lookupEnvKeyOrDefault(env_key string, default_value string) string {
 	return val
 }
 
-func init_global_config() {
-	conf.rsbe_url = lookupEnvKeyOrDefault("FLOW_UI_RSBE_URL", "http://localhost:3000")
-	conf.rsbe_user = lookupEnvKeyOrDefault("FLOW_UI_RSBE_USER", "foo")
-	conf.rsbe_password = lookupEnvKeyOrDefault("FLOW_UI_RSBE_PASSWORD", "bar")
-}
-
 func Get(path string) (resp *http.Response, err error) {
-	url := fmt.Sprintf("%s%s", conf.rsbe_url, path)
+	url := fmt.Sprintf("%s%s", conf.BaseURL, path)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.SetBasicAuth(conf.rsbe_user, conf.rsbe_password)
+	req.SetBasicAuth(conf.User, conf.Password)
 	return client.Do(req)
 }
 
 func GetBodyTextString(path string) (s string, err error) {
 	s = ""
-	url := fmt.Sprintf("%s%s", conf.rsbe_url, path)
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	req.SetBasicAuth(conf.rsbe_user, conf.rsbe_password)
 
-	var resp *http.Response
-	resp, err = client.Do(req)
+	resp, err := Get(path)
 	if err != nil {
 		return s, err
 	}
@@ -68,19 +57,5 @@ func GetBodyTextString(path string) (s string, err error) {
 		return s, err
 	}
 
-	
 	return string(bodyText), nil
-
-	// resp, err := Get(path)
-	// if err != nil {
-	// 	return partner, err
-	// }
-
-	// bodyText, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	return partner, err
-	// }
-
-	// s := string(bodyText)
-
 }
