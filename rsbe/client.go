b7a8c4479b36d041e0bf0a1157e5ebf7b2fd77c4
jgpawletko <jgpawletko@gmail.com>
2020-06-27 00:55:49 -0400

    add comments; add POST, DELETE methods

--- rsbe/client.go
+++ rsbe/client.go
@@ -1,6 +1,7 @@
 package rsbe
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -33,6 +34,11 @@ func Get(path string) (resp *http.Response, err error) {
 	return client.Do(req)
 }
 
+// TODO: change fn name? GetBodyText?
+// TODO: do you need intermediate bodyText variable, or
+//       or can you just use variable "s"?
+//       add status code check
+//       also might want to just merge all of this into Get?
 func GetBodyTextString(path string) (s string, err error) {
 	s = ""
 
@@ -40,6 +46,7 @@ func GetBodyTextString(path string) (s string, err error) {
 	if err != nil {
 		return s, err
 	}
+	defer resp.Body.Close()
 
 	bodyText, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -47,4 +54,60 @@ func GetBodyTextString(path string) (s string, err error) {
 	}
 
 	return string(bodyText), nil
+}
+
+func Post(path string, data []byte) (resp *http.Response, err error) {
+	url := fmt.Sprintf("%s%s", conf.BaseURL, path)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return resp, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(conf.User, conf.Password)
+
+	return client.Do(req)
+}
+
+// TODO: refactor: extract bodyText conversion to string?
+func PostReturnBody(path string, data []byte) (body []byte, err error) {
+	resp, err := Post(path, data)
+	if err != nil {
+		return body, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 201 {
+		return body, fmt.Errorf("unexpected status code: %s", resp.Status)
+	}
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return body, err
+	}
+
+	return body, nil
+}
+
+// TODO: refactor: extract bodyText conversion to string?
+func Delete(path string) (err error) {
+	url := fmt.Sprintf("%s%s", conf.BaseURL, path)
+
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(conf.User, conf.Password)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 204 {
+		return fmt.Errorf("unexpected status code: %s", resp.Status)
+	}
+
+	return nil
 }
