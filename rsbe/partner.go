cd2afd1f17b72d52a73b1f3b3925c92c1cab8b7a
jgpawletko <jgpawletko@gmail.com>
2020-06-27 00:57:18 -0400

    change type name, add ParterCreate, PartnerDelete

--- rsbe/partner.go
+++ rsbe/partner.go
@@ -14,16 +14,16 @@ type PartnerListEntry struct {
 	URL        string `json:"url"`
 }
 
-type PartnerShowEntry struct {
-	ID             string `json:"id"`
-	Code           string `json:"code"`
-	Name           string `json:"name"`
-	Created_at     string `json:"created_at"`
-	Updated_at     string `json:"updated_at"`
-	PartnersURL    string `json:"partners_url"`
-	CollectionsURL string `json:"colls_url"`
-	LockVersion    int    `json:"lock_version"`
-	RelPath        string `json:"rel_path"`
+type PartnerEntry struct {
+	ID             string `json:"id,omitempty"`
+	Code           string `json:"code,omitempty"`
+	Name           string `json:"name,omitempty"`
+	Created_at     string `json:"created_at,omitempty"`
+	Updated_at     string `json:"updated_at,omitempty"`
+	PartnersURL    string `json:"partners_url,omitempty"`
+	CollectionsURL string `json:"colls_url,omitempty"`
+	LockVersion    int    `json:"lock_version,omitempty"`
+	RelPath        string `json:"rel_path,omitempty"`
 }
 
 func PartnerList() (partners []PartnerListEntry, err error) {
@@ -41,7 +41,7 @@ func PartnerList() (partners []PartnerListEntry, err error) {
 	return partners, nil
 }
 
-func PartnerGet(id string) (partner PartnerShowEntry, err error) {
+func PartnerGet(id string) (partner PartnerEntry, err error) {
 	path := "/api/v0/partners/" + id
 
 	s, err := GetBodyTextString(path)
@@ -57,6 +57,38 @@ func PartnerGet(id string) (partner PartnerShowEntry, err error) {
 	return partner, nil
 }
 
+func PartnerCreate(p *PartnerEntry) (err error) {
+	path := "/api/v0/partners"
+
+	data, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+
+	body, err := PostReturnBody(path, data)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, p)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s\n", body)
+	return nil
+}
+
+func PartnerDelete(id string) (err error) {
+	path := "/api/v0/partners/" + id
+
+	err = Delete(path)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (e PartnerListEntry) ToString() string {
 	s := fmt.Sprintf("ID: %s, Code: %s, Name: %s, Created_at: %s , Updated_at: %s, URL: %s",
 		e.ID, e.Code, e.Name, e.Created_at, e.Updated_at, e.URL)
@@ -64,7 +96,7 @@ func (e PartnerListEntry) ToString() string {
 	return s
 }
 
-func (e PartnerShowEntry) ToString() string {
+func (e PartnerEntry) ToString() string {
 	s := fmt.Sprintf("ID: %s, Code: %s, Name: %s, Created_at: %s , Updated_at: %s, PartnersURL: %s, CollectionsURL: %s, LockVersion: %d, RelPath: %s",
 		e.ID, e.Code, e.Name, e.Created_at, e.Updated_at, e.PartnersURL, e.CollectionsURL, e.LockVersion, e.RelPath)
 
