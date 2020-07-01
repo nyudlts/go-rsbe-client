8bf960cd318d1cd6be8540071c969c5078f97962
jgpawletko <jgpawletko@gmail.com>
2020-07-01 15:19:27 -0400

    update Collection
    * use renamed GetBody method
    * add "omitempty" to JSON tags
    * add Create and Delete methods

--- rsbe/collection.go
+++ rsbe/collection.go
@@ -17,32 +17,32 @@ type CollectionListEntry struct {
 	PartnerURL string `json:"partner_url"`
 }
 
-type CollectionShowEntry struct {
-	ID              string `json:"id"`
-	PartnerID       string `json:"partner_id"`
-	Code            string `json:"code"`
-	Name            string `json:"name"`
-	CollType        string `json:"coll_type"`
-	Created_at      string `json:"created_at"`
-	Updated_at      string `json:"updated_at"`
-	Quota           int    `json:"quota"`
-	ReadyForContent bool   `json:"ready_for_content"`
-	PartnerURL      string `json:"partner_url"`
-	SEsURL          string `json:"ses_url"`
-	IEsURL          string `json:"ies_url"`
-	LockVersion     int    `json:"lock_version"`
-	RelPath         string `json:"rel_path"`
+type CollectionEntry struct {
+	ID              string `json:"id,omitempty"`
+	PartnerID       string `json:"partner_id,omitempty"`        // REQUIRED
+	Code            string `json:"code,omitempty"`              // REQUIRED
+	Name            string `json:"name,omitempty"`              // optional
+	CollType        string `json:"coll_type,omitempty"`         // REQUIRED (origin, virtual)
+	Quota           int    `json:"quota,omitempty"`             // REQUIRED
+	RelPath         string `json:"rel_path,omitempty"`          // REQUIRED
+	ReadyForContent bool   `json:"ready_for_content,omitempty"` // optional
+	PartnerURL      string `json:"partner_url,omitempty"`
+	SEsURL          string `json:"ses_url,omitempty"`
+	IEsURL          string `json:"ies_url,omitempty"`
+	LockVersion     int    `json:"lock_version,omitempty"`
+	Created_at      string `json:"created_at,omitempty"`
+	Updated_at      string `json:"updated_at,omitempty"`
 }
 
 func PartnerCollectionsList(partnerID string) (collections []CollectionListEntry, err error) {
 	path := fmt.Sprintf("/api/v0/partners/%s/colls", partnerID)
 
-	s, err := GetBodyTextString(path)
+	body, err := GetBody(path)
 	if err != nil {
 		return nil, err
 	}
 
-	err = json.Unmarshal([]byte(s), &collections)
+	err = json.Unmarshal(body, &collections)
 	if err != nil {
 		return nil, err
 	}
@@ -50,20 +50,67 @@ func PartnerCollectionsList(partnerID string) (collections []CollectionListEntry
 	return collections, nil
 }
 
-func CollectionGet(id string) (collection CollectionShowEntry, err error) {
+func CollectionGet(id string) (collection CollectionEntry, err error) {
 	path := fmt.Sprintf("/api/v0/colls/%s", id)
 
-	s, err := GetBodyTextString(path)
+	body, err := GetBody(path)
 	if err != nil {
 		return collection, err
 	}
 
-	err = json.Unmarshal([]byte(s), &collection)
+	err = json.Unmarshal(body, &collection)
 	if err != nil {
 		return collection, err
 	}
 
 	return collection, nil
+}
+
+func CollectionCreate(c *CollectionEntry) (err error) {
+	path := "/api/v0/colls"
+
+	data, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	body, err := PostReturnBody(path, data)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CollectionUpdate(c *PartnerEntry) (err error) {
+	path := "/api/v0/colls/" + c.ID
+
+	data, err := json.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	err = Put(path, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CollectionDelete(id string) (err error) {
+	path := "/api/v0/colls/" + id
+
+	err = Delete(path)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (e CollectionListEntry) ToString() string {
@@ -73,7 +120,7 @@ func (e CollectionListEntry) ToString() string {
 	return s
 }
 
-func (e CollectionShowEntry) ToString() string {
+func (e CollectionEntry) ToString() string {
 	s := fmt.Sprintf("ID: %s, PartnerID: %s, Code: %s, Name: %s, CollType: %s, Created_at: %s , Updated_at: %s, Quota: %d, ReadyForContent: %v, PartnerURL: %s, SEsURL: %s, IEsURL: %s, LockVersion: %d, RelPath: %s", e.ID, e.PartnerID, e.Code, e.Name, e.CollType, e.Created_at, e.Updated_at, e.Quota, e.ReadyForContent, e.PartnerURL, e.SEsURL, e.IEsURL, e.LockVersion, e.RelPath)
 
 	return s
