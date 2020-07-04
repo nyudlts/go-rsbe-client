2fd1fda8765ed5e5826b33b410354a82bb906d1c
jgpawletko <jgpawletko@gmail.com>
2020-07-04 01:39:40 -0400

    update partner.go
    * Remove snake case from struct attributes ("_at" to "At")
    * Change return value for PartnerList() to []PartnerEntry

--- rsbe/partner.go
+++ rsbe/partner.go
@@ -6,27 +6,27 @@ import (
 )
 
 type PartnerListEntry struct {
-	ID         string `json:"id"`
-	Code       string `json:"code"`
-	Name       string `json:"name"`
-	Created_at string `json:"created_at"`
-	Updated_at string `json:"updated_at"`
-	URL        string `json:"url"`
+	ID        string `json:"id"`
+	Code      string `json:"code"`
+	Name      string `json:"name"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	URL       string `json:"url"`
 }
 
 type PartnerEntry struct {
 	ID             string `json:"id,omitempty"`
 	Code           string `json:"code,omitempty"`
 	Name           string `json:"name,omitempty"`
-	Created_at     string `json:"created_at,omitempty"`
-	Updated_at     string `json:"updated_at,omitempty"`
+	CreatedAt      string `json:"created_at,omitempty"`
+	UpdatedAt      string `json:"updated_at,omitempty"`
 	PartnersURL    string `json:"partners_url,omitempty"`
 	CollectionsURL string `json:"colls_url,omitempty"`
 	LockVersion    int    `json:"lock_version,omitempty"`
 	RelPath        string `json:"rel_path,omitempty"`
 }
 
-func PartnerList() (partners []PartnerListEntry, err error) {
+func PartnerList() (partners []PartnerEntry, err error) {
 
 	body, err := GetBody("/api/v0/partners")
 	if err != nil {
@@ -57,7 +57,23 @@ func PartnerGet(id string) (partner PartnerEntry, err error) {
 	return partner, nil
 }
 
-func PartnerCreate(p *PartnerEntry) (err error) {
+func (p *PartnerEntry) Get() (err error) {
+	path := "/api/v0/partners/" + p.ID
+
+	body, err := GetBody(path)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *PartnerEntry) Create() (err error) {
 	path := "/api/v0/partners"
 
 	data, err := json.Marshal(p)
@@ -78,7 +94,7 @@ func PartnerCreate(p *PartnerEntry) (err error) {
 	return nil
 }
 
-func PartnerUpdate(p *PartnerEntry) (err error) {
+func (p *PartnerEntry) Update() (err error) {
 	path := "/api/v0/partners/" + p.ID
 
 	data, err := json.Marshal(p)
@@ -104,16 +120,20 @@ func PartnerDelete(id string) (err error) {
 	return nil
 }
 
+func (p *PartnerEntry) Delete() (err error) {
+	return PartnerDelete(p.ID)
+}
+
 func (e PartnerListEntry) ToString() string {
-	s := fmt.Sprintf("ID: %s, Code: %s, Name: %s, Created_at: %s , Updated_at: %s, URL: %s",
-		e.ID, e.Code, e.Name, e.Created_at, e.Updated_at, e.URL)
+	s := fmt.Sprintf("ID: %s, Code: %s, Name: %s, CreatedAt: %s , UpdatedAt: %s, URL: %s",
+		e.ID, e.Code, e.Name, e.CreatedAt, e.UpdatedAt, e.URL)
 
 	return s
 }
 
 func (e PartnerEntry) ToString() string {
-	s := fmt.Sprintf("ID: %s, Code: %s, Name: %s, Created_at: %s , Updated_at: %s, PartnersURL: %s, CollectionsURL: %s, LockVersion: %d, RelPath: %s",
-		e.ID, e.Code, e.Name, e.Created_at, e.Updated_at, e.PartnersURL, e.CollectionsURL, e.LockVersion, e.RelPath)
+	s := fmt.Sprintf("ID: %s, Code: %s, Name: %s, CreatedAt: %s , UpdatedAt: %s, PartnersURL: %s, CollectionsURL: %s, LockVersion: %d, RelPath: %s",
+		e.ID, e.Code, e.Name, e.CreatedAt, e.UpdatedAt, e.PartnersURL, e.CollectionsURL, e.LockVersion, e.RelPath)
 
 	return s
 }
