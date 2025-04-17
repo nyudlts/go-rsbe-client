47d3e8ea054d15658f6fd2d9ce54ee9fa3d70d05
Joseph G. Pawletko <jgpawletko@gmail.com>
2025-04-17 14:20:13 -0400

    add classification field to Collection ToString()

--- rsbe/collection_test.go
+++ rsbe/collection_test.go
@@ -2,22 +2,24 @@ package rsbe
 
 import (
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
 var collectionListEntry = CollectionListEntry{
-	ID:          "b9612d5d-619a-4ceb-b620-d816e4b4340b",
-	PartnerID:   "e6517775-6277-4e25-9373-ee7738e820b5",
-	OwnerID:     "1ca830b5-6a2b-43f9-b6bc-4dfeac3ee178",
-	Code:        "test",
-	DisplayCode: "T.C",
-	Name:        "Test Collection",
-	Type:        "origin",
-	CreatedAt:   "2020-05-30T01:58:38.431Z",
-	UpdatedAt:   "2024-08-19T14:53:30.432Z",
-	URL:         "http://localhost:3000/api/v0/colls/b9612d5d-619a-4ceb-b620-d816e4b4340b",
-	PartnerURL:  "http://localhost:3000/api/v0/partners/e6517775-6277-4e25-9373-ee7738e820b5",
-	OwnerURL:    "http://localhost:3000/api/v0/owners/1ca830b5-6a2b-43f9-b6bc-4dfeac3ee178",
+	ID:             "b9612d5d-619a-4ceb-b620-d816e4b4340b",
+	PartnerID:      "e6517775-6277-4e25-9373-ee7738e820b5",
+	OwnerID:        "1ca830b5-6a2b-43f9-b6bc-4dfeac3ee178",
+	Code:           "test",
+	DisplayCode:    "T.C",
+	Name:           "Test Collection",
+	Type:           "origin",
+	Classification: "restricted",
+	CreatedAt:      "2020-05-30T01:58:38.431Z",
+	UpdatedAt:      "2024-08-19T14:53:30.432Z",
+	URL:            "http://localhost:3000/api/v0/colls/b9612d5d-619a-4ceb-b620-d816e4b4340b",
+	PartnerURL:     "http://localhost:3000/api/v0/partners/e6517775-6277-4e25-9373-ee7738e820b5",
+	OwnerURL:       "http://localhost:3000/api/v0/owners/1ca830b5-6a2b-43f9-b6bc-4dfeac3ee178",
 }
 
 var collectionShow = CollectionEntry{
@@ -188,6 +190,7 @@ func TestCollectionUpdateFunc(t *testing.T) {
 	collectionToCreate.DisplayCode = "D.B"
 	collectionToCreate.Name = "Dog Biscuit"
 	collectionToCreate.Type = "origin"
+	collectionToCreate.Classification = "restricted"
 	collectionToCreate.RelPath = "content/dlts/dogbiscuit"
 	collectionToCreate.Quota = 0
 	collectionToCreate.ReadyForContent = false
@@ -206,6 +209,7 @@ func TestCollectionUpdateFunc(t *testing.T) {
 	checkStringContains(t, collectionToCreate.DisplayCode, "D.B")
 	checkStringContains(t, collectionToCreate.Name, "Dog Biscuit")
 	checkStringContains(t, collectionToCreate.Type, "origin")
+	checkStringContains(t, collectionToCreate.Classification, "restricted")
 	checkStringContains(t, collectionToCreate.RelPath, "content/dlts/dogbiscuit")
 	if collectionToCreate.Quota != 0 {
 		t.Errorf("Quota was not updated: got: %d", collectionToCreate.Quota)
@@ -246,4 +250,70 @@ func TestCollectionDeleteFunc(t *testing.T) {
 			t.Errorf("err was nil")
 		}
 	})
+}
+
+func TestCollectionEntryToString(t *testing.T) {
+	setupLocalhostClient()
+	err := collectionToCreate.Create()
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+	defer collectionToCreate.Delete()
+
+	err = collectionToCreate.Get()
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	s := collectionToCreate.ToString()
+	if s == "" {
+		t.Errorf("ToString returned an empty string")
+	}
+	checkStringContains(t, s, collectionToCreate.ID)
+	checkStringContains(t, s, collectionToCreate.PartnerID)
+	checkStringContains(t, s, collectionToCreate.OwnerID)
+	checkStringContains(t, s, collectionToCreate.Code)
+	checkStringContains(t, s, collectionToCreate.DisplayCode)
+	checkStringContains(t, s, collectionToCreate.Name)
+	checkStringContains(t, s, collectionToCreate.Type)
+	checkStringContains(t, s, collectionToCreate.Classification)
+	checkStringContains(t, s, collectionToCreate.CreatedAt)
+	checkStringContains(t, s, collectionToCreate.UpdatedAt)
+	checkStringContains(t, s, collectionToCreate.PartnerURL)
+	checkStringContains(t, s, collectionToCreate.OwnerURL)
+	checkStringContains(t, s, collectionToCreate.SEsURL)
+	checkStringContains(t, s, collectionToCreate.IEsURL)
+	checkStringContains(t, s, collectionToCreate.RelPath)
+	checkStringContains(t, s, strconv.Itoa(collectionToCreate.LockVersion))
+}
+
+func TestCollectionListEntryToString(t *testing.T) {
+	setupLocalhostClient()
+	collList, err := PartnerCollectionList("e6517775-6277-4e25-9373-ee7738e820b5")
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	if len(collList) != 1 {
+		t.Errorf("Mismatch: want: 1, got: %d", len(collList))
+		t.FailNow()
+	}
+
+	collListEntry := collList[0]
+	s := collListEntry.ToString()
+	if s == "" {
+		t.Errorf("ToString returned an empty string")
+	}
+	checkStringContains(t, s, collListEntry.ID)
+	checkStringContains(t, s, collListEntry.PartnerID)
+	checkStringContains(t, s, collListEntry.OwnerID)
+	checkStringContains(t, s, collListEntry.Code)
+	checkStringContains(t, s, collListEntry.DisplayCode)
+	checkStringContains(t, s, collListEntry.Name)
+	checkStringContains(t, s, collListEntry.Type)
+	checkStringContains(t, s, collListEntry.Classification)
+	checkStringContains(t, s, collListEntry.CreatedAt)
+	checkStringContains(t, s, collListEntry.UpdatedAt)
+	checkStringContains(t, s, collListEntry.PartnerURL)
+	checkStringContains(t, s, collListEntry.OwnerURL)
 }
